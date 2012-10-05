32e146044b32b9c295c6f4b2bd6ef9ba81747e53
Niklas Hambüchen <mail@nh2.me>
2012-10-05 19:04:26 +0200

    Factor out argument parser

--- main.py
+++ main.py
@@ -5,11 +5,15 @@ import argparse
 import pubres
 
 
-def main():
+def make_parser():
     parser = argparse.ArgumentParser(description='pubres server.')
     parser.add_argument('--host', type=str, default='localhost', help='host to listen on (omit for localhost)')
     parser.add_argument('--port', type=int, default=5555, help='port to listen on (default is 5555)')
-    args = parser.parse_args()
+    return parser
+
+
+def main():
+    args = make_parser().parse_args()
 
     pubres.Server(host=args.host, port=args.port).serve_forever()
 
