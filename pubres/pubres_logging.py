b288c02482cc8327d49ba53ed9ac744b4bc04bdc
Niklas Hambüchen <mail@nh2.me>
2012-10-15 19:24:22 +0100

    pubres_logging: Cllow custom log level and handler

--- pubres/pubres_logging.py
+++ pubres/pubres_logging.py
@@ -8,9 +8,15 @@ import logging
 PUBRES_LOGGER_NAME = 'pubres'
 
 
-def setup_logging(level):
+def setup_logging(level=logging.INFO, handler=None):
+    """Configures a logger called 'pubres'.
+
+    Optionally takes:
+    - a log level (either a string like 'INFO' or numeric constant like logging.INFO)
+    - a custom log handler to process the `LogRecords`.
+    """
     # Check if the log level string is one defined in logging
-    numeric_level = getattr(logging, level.upper())
+    numeric_level = getattr(logging, level.upper()) if isinstance(level, str) else level
     if not isinstance(numeric_level, int):
         raise ValueError('Invalid log level: %s' % level)
 
@@ -19,8 +25,9 @@ def setup_logging(level):
     logger = logging.getLogger(PUBRES_LOGGER_NAME)
     logger.setLevel(numeric_level)
 
-    handler = logging.StreamHandler()
-    handler.setLevel(numeric_level)
+    if handler is None:
+        handler = logging.StreamHandler()
+        handler.setLevel(numeric_level)
 
     formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
     handler.setFormatter(formatter)
