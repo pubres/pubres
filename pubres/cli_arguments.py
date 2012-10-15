31abeb927a30021ce332f9e462de91a66c206f7d
Niklas Hambüchen <mail@nh2.me>
2012-10-15 16:48:42 +0100

    Change default host from "localhost" to 127.0.0.1

--- pubres/cli_arguments.py
+++ pubres/cli_arguments.py
@@ -3,7 +3,7 @@ import argparse
 
 def make_parser():
     parser = argparse.ArgumentParser(description='pubres server.')
-    parser.add_argument('--host', type=str, default='localhost', help='host to listen on (omit for localhost)')
+    parser.add_argument('--host', type=str, default='127.0.0.1', help='host to listen on (omit for localhost)')
     parser.add_argument('--port', type=int, default=5555, help='port to listen on (default is 5555)')
     parser.add_argument('--loglevel', type=str, default='INFO', help='Log level. DEBUG, INFO, WARNING, ERROR or CRITICAL.')
     return parser
